dad139ccc7566dacffc9f1acc5767af18e5cf523
Bertan Ulusoy <bertan.ulusoy@onur.net>
2019-06-29 10:22:36 +0300

    es2016 babel preset problem solved with npm install --save-dev babel-preset-es2016.

--- src/components/Onset.vue
+++ src/components/Onset.vue
@@ -1,15 +1,27 @@
 <template>
   <div>
-      Onset of booking goes here!
+    <datetime format="YYYY-MM-DD" v-model='dob'></datetime>
+    <button @click="alertVal">Alert current date value</button>
+    Onset of booking goes here!
   </div>
 </template>
 
 <script>
+import datetime from 'vuejs-datetimepicker'
+
 export default {
   name: 'Onset',
+  components: {
+    datetime
+  },
   data () {
     return {
       msg: 'Welcome to Booking Vue.js App'
+    }
+  },
+  methods: {
+    alertVal () {
+      alert(this.dob)
     }
   }
 }
